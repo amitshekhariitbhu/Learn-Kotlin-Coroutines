77315c883cebb338853eb0f8706a0ed8d185aa87
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2024-09-07 12:14:55 +0530

    Remove coroutineScope

--- app/src/main/java/me/amitshekhar/learn/kotlin/coroutines/ui/retrofit/parallel/ParallelNetworkCallsViewModel.kt
+++ app/src/main/java/me/amitshekhar/learn/kotlin/coroutines/ui/retrofit/parallel/ParallelNetworkCallsViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import me.amitshekhar.learn.kotlin.coroutines.data.api.ApiHelper
 import me.amitshekhar.learn.kotlin.coroutines.data.local.DatabaseHelper
@@ -13,37 +13,34 @@ import me.amitshekhar.learn.kotlin.coroutines.data.model.ApiUser
 import me.amitshekhar.learn.kotlin.coroutines.ui.base.UiState
 
 class ParallelNetworkCallsViewModel(
-    private val apiHelper: ApiHelper,
-    private val dbHelper: DatabaseHelper
+    private val apiHelper: ApiHelper, private val dbHelper: DatabaseHelper
 ) : ViewModel() {
 
     private val uiState = MutableLiveData<UiState<List<ApiUser>>>()
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        uiState.postValue(UiState.Error("exception handler: $e"))
+    }
 
     init {
         fetchUsers()
     }
 
     private fun fetchUsers() {
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             uiState.postValue(UiState.Loading)
-            try {
-                // coroutineScope is needed, else in case of any network error, it will crash
-                coroutineScope {
-                    val usersFromApiDeferred = async { apiHelper.getUsers() }
-                    val moreUsersFromApiDeferred = async { apiHelper.getMoreUsers() }
 
-                    val usersFromApi = usersFromApiDeferred.await()
-                    val moreUsersFromApi = moreUsersFromApiDeferred.await()
+            val usersFromApiDeferred = async { apiHelper.getUsers() }
+            val moreUsersFromApiDeferred = async { apiHelper.getMoreUsers() }
 
-                    val allUsersFromApi = mutableListOf<ApiUser>()
-                    allUsersFromApi.addAll(usersFromApi)
-                    allUsersFromApi.addAll(moreUsersFromApi)
+            val usersFromApi = usersFromApiDeferred.await()
+            val moreUsersFromApi = moreUsersFromApiDeferred.await()
 
-                    uiState.postValue(UiState.Success(allUsersFromApi))
-                }
-            } catch (e: Exception) {
-                uiState.postValue(UiState.Error("Something Went Wrong"))
-            }
+            val allUsersFromApi = mutableListOf<ApiUser>()
+            allUsersFromApi.addAll(usersFromApi)
+            allUsersFromApi.addAll(moreUsersFromApi)
+
+            uiState.postValue(UiState.Success(allUsersFromApi))
         }
     }
 
